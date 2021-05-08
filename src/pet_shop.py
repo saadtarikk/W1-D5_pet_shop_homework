9f47d28e3676e10d5e60d7438015f9fccc296929
[master] <saad.tarik@outlook.com>
2021-05-08 15:10:58 +0100

    removed puesdocode comments

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -6,31 +6,31 @@ def get_pet_shop_name(pet_shop):
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
-def add_or_remove_cash(pet_shop, enter_amount): # test wants us to return totalcash in petshop["admin"] dict 
-    pet_shop["admin"]["total_cash"] += enter_amount # we use += to add or subsrtact value in variable enter_amount it will perform + and - because -10 + 100 is still = 90
+def add_or_remove_cash(pet_shop, enter_amount): 
+    pet_shop["admin"]["total_cash"] += enter_amount
     
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
 
 def increase_pets_sold(pet_shop, enter_amount):
-    # pet_shop["admin"]["pets_sold"] += enter_amount
-    pet_shop["admin"]["pets_sold"] = enter_amount + get_pets_sold(pet_shop)
+    pet_shop["admin"]["pets_sold"] += enter_amount
+    # pet_shop["admin"]["pets_sold"] = enter_amount + get_pets_sold(pet_shop)
 
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
 
 def get_pets_by_breed(pet_shop, what_breed):
-    breed = [] # ==>    we create a variable here because we want to get back all the pets in dictionary by name
-    for pet in pet_shop["pets"]: # means it goes through every pet in list of pet dictionary in pet_shop dictionary
-        if pet["breed"] == what_breed: # if pet[breed] in list of pet dictionary == to what_gread which is a given variable
-            breed.append(pet)# add pet[breed] into empty breed =[] variable
-    return breed # return the value breed[] variable
+    breed = [] 
+    for pet in pet_shop["pets"]: 
+        if pet["breed"] == what_breed: 
+            breed.append(pet)
+    return breed 
         
 def find_pet_by_name(pet_shop, pet_name):
 
-    for pet in pet_shop["pets"]: #for every pet in pet_shop dict "pets" list if its == pet_name 
+    for pet in pet_shop["pets"]: 
         if pet["name"] == pet_name:
-            return pet # return value
+            return pet
 
 def remove_pet_by_name(pet_shop, pet_name):
     # for pet in pet_shop["pets"]:
@@ -39,12 +39,9 @@ def remove_pet_by_name(pet_shop, pet_name):
             # return pet_shop["pets"]
     
     # pet = find_pet_by_name(pet_shop, pet_name)
+    # pet_shop["pets"].remove(pet)
      
     pet_shop["pets"].remove(find_pet_by_name(pet_shop, pet_name))
-
-    
-
-
 
 def add_pet_to_stock(pet_shop, new_pet):
     pet_shop["pets"].append(new_pet)
@@ -56,10 +53,10 @@ def remove_customer_cash(customers, give_amount):
     customers["cash"] -= give_amount
 
 def get_customer_pet_count(customers):
-    return len(customers["pets"]) #why do we use len here but in when getting customer cash
+    return len(customers["pets"])
 
-def add_pet_to_customer(customers, new_pet):
-    customers["pets"].append(new_pet) # we need to tell function where to append inside customers list.
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet) # we need to tell function where to append inside customers list.
 
 
 def customer_can_afford_pet(customer, new_pet):
@@ -76,17 +73,4 @@ def sell_pet_to_customer(pet_shop, pet, customer):
         add_or_remove_cash(pet_shop, pet["price"])
         add_pet_to_customer(customer, pet)
         increase_pets_sold(pet_shop, 1)
-            
-
-
-  
-
-        
-                                         #self.assertEqual(1, get_customer_pet_count(customer))
-                                      # self.assertEqual(1, get_pets_sold(self.cc_pet_shop))
-                                      # self.assertEqual(100, get_customer_cash(customer))
-                                      # self.assertEqual(1900, get_total_cash(self.cc_pet_shop))
-                                        # "name": "Alice",
-                                        # "pets": [],
-                                        # "cash": 1000
 
